506c6359c382e78b3c7af569b418849468b2b4d9
Javier Negre <javi.negre@gmail.com>
2014-01-27 23:34:40 +0100

    Send data from controller

--- demo.php
+++ demo.php
@@ -6,6 +6,11 @@
     <body>
 
     </body>
+	<script>
+		var view = {
+			'chartData': <?php echo json_encode($data); ?>
+		};
+	</script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.1/d3.min.js"></script>
     <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
     <script src="/projects/linechart/js/charts.js"></script>
